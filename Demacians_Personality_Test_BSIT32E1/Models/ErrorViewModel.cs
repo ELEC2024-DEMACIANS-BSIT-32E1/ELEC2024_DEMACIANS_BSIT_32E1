0545755b2964cae48691e266d94ede037a3029c3
Gabriel <gabrielpabalesalazar@gmail.com>
2024-05-08 19:35:49 +0800

    Add project files.

---
+++ Demacians_Personality_Test_BSIT32E1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Demacians_Personality_Test_BSIT32E1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
