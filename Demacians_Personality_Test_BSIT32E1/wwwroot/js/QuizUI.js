38e0feb5c44ce8788135349f62bd02b7ebf532bd
GEROLIA_JohnGabriel_BSIT32E1 <johngabrielgerolia05@gmail.com>
2024-06-02 00:51:41 +0800

    95% done. doing Next button at lower right

--- Demacians_Personality_Test_BSIT32E1/wwwroot/js/QuizUI.js
+++ Demacians_Personality_Test_BSIT32E1/wwwroot/js/QuizUI.js
@@ -1,5 +1,7 @@
-﻿@*
-    For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-*@
-@{
-}
+﻿document.querySelectorAll('.scale-button').forEach(button => {
+    button.addEventListener('click', () => {
+        document.querySelectorAll('.scale-button').forEach(btn => btn.classList.remove('selected'));
+        button.classList.add('selected');
+        console.log('Selected value:', button.getAttribute('data-value'));
+    });
+});
