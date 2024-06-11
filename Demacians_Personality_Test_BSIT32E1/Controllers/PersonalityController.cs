bdde396b32fc66e16566ed37965ff13d68574dd5
Tabin_Jasper_BSIT32E1 <tabinjasper@gmail.com>
2024-06-11 23:49:28 +0800

    Added the controller for architect & changed the rout for architect also.

--- Demacians_Personality_Test_BSIT32E1/Controllers/PersonalityController.cs
+++ Demacians_Personality_Test_BSIT32E1/Controllers/PersonalityController.cs
@@ -23,6 +23,10 @@ namespace Demacians_Personality_Test_BSIT32E1.Controllers
             return View();
         }
 
+        public IActionResult Architect()
+        {
+            return View();
+        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
