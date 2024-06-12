8b168869b5ea1f74223d9b954f4b21e9d77fe6c9
GerodiazRogel-BSIT32E1 <gerodiazrogel1@gmail.com>
2024-06-12 22:33:23 +0800

    removing footer in the PersonalitPage

--- Demacians_Personality_Test_BSIT32E1/Program.cs
+++ Demacians_Personality_Test_BSIT32E1/Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
 
-    pattern: "{controller=AboutUs}/{action=AboutUs}/{id?}");
+    pattern: "{controller=Personality}/{action=PersonalityPage}/{id?}");
 
 app.Run();
