a13b150e8a08bb0bb502b40a2e4810fd10c94ab9
smarty0917 <martintristanmreyes@gmail.com>
2024-06-13 17:11:16 +0800

    Added redirect to personality page from quiz Added Protagonist to quiz result

--- Demacians_Personality_Test_BSIT32E1/Controllers/PersonalityController.cs
+++ Demacians_Personality_Test_BSIT32E1/Controllers/PersonalityController.cs
@@ -13,6 +13,16 @@ namespace Demacians_Personality_Test_BSIT32E1.Controllers
             _logger = logger;
         }
 
+        [HttpPost]
+        public IActionResult Direct(int sum)
+        {
+            if (sum >= 60 && sum <= 82)
+            {
+                return Json(new { action = "Protagonist" });
+            }
+            return Json(new { action = "PersonalityPage" });
+        }
+
         public IActionResult PersonalityPage()
         {
             return View();
